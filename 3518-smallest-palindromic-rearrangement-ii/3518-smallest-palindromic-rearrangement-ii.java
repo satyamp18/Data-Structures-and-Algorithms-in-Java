2d8b97b296f0bbe4ba68858757c27fb5de557fee
Satyam Pandey <satyampandey200317@gmail.com>
2026-07-29 11:17:38 +0530

    Time: 19 ms (74.19%), Space: 47 MB (90.32%) - LeetHub

---
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
@@ -0,0 +1,103 @@
+class Solution {
+
+    long LIMIT;
+
+    public String smallestPalindrome(String s, int k) {
+
+        LIMIT = k;
+
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int[] half = new int[26];
+        int halfLen = 0;
+        char mid = 0;
+
+        for (int i = 0; i < 26; i++) {
+            half[i] = freq[i] / 2;
+            halfLen += half[i];
+            if ((freq[i] & 1) == 1)
+                mid = (char) ('a' + i);
+        }
+
+        if (count(half, halfLen) < k)
+            return "";
+
+        StringBuilder left = new StringBuilder();
+
+        for (int pos = 0; pos < halfLen; pos++) {
+
+            for (int c = 0; c < 26; c++) {
+
+                if (half[c] == 0)
+                    continue;
+
+                half[c]--;
+
+                long ways = count(half, halfLen - pos - 1);
+
+                if (ways >= k) {
+                    left.append((char) ('a' + c));
+                    break;
+                }
+
+                k -= ways;
+                half[c]++;
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        ans.append(left);
+
+        if (mid != 0)
+            ans.append(mid);
+
+        ans.append(new StringBuilder(left).reverse());
+
+        return ans.toString();
+    }
+
+    private long count(int[] cnt, int total) {
+
+        long res = 1;
+        int remain = total;
+
+        for (int i = 0; i < 26; i++) {
+
+            int x = cnt[i];
+            if (x == 0)
+                continue;
+
+            res *= comb(remain, x);
+
+            if (res > LIMIT)
+                return LIMIT;
+
+            remain -= x;
+        }
+
+        return Math.min(res, LIMIT);
+    }
+
+    private long comb(int n, int r) {
+
+        if (r > n)
+            return 0;
+
+        r = Math.min(r, n - r);
+
+        long ans = 1;
+
+        for (int i = 1; i <= r; i++) {
+
+            ans = ans * (n - r + i) / i;
+
+            if (ans > LIMIT)
+                return LIMIT;
+        }
+
+        return ans;
+    }
+}
