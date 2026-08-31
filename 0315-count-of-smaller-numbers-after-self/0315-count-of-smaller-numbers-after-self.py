7cf7952b2301668408d43c92248f98f5970e2c7f
Satyam Pandey <satyampandey200317@gmail.com>
2026-08-31 23:50:53 +0530

    Time: 1085 ms (61.31%), Space: 112.5 MB (13.17%) - LeetHub

---
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -0,0 +1,44 @@
+class Solution:
+    def countSmaller(self, nums):
+        n = len(nums)
+        result = [0] * n
+        arr = [(nums[i], i) for i in range(n)]
+
+        def merge_sort(left, right):
+            if right - left <= 1:
+                return
+
+            mid = (left + right) // 2
+
+            merge_sort(left, mid)
+            merge_sort(mid, right)
+
+            temp = []
+            i = left
+            j = mid
+            smaller = 0
+
+            while i < mid and j < right:
+                if arr[j][0] < arr[i][0]:
+                    temp.append(arr[j])
+                    smaller += 1
+                    j += 1
+                else:
+                    result[arr[i][1]] += smaller
+                    temp.append(arr[i])
+                    i += 1
+
+            while i < mid:
+                result[arr[i][1]] += smaller
+                temp.append(arr[i])
+                i += 1
+
+            while j < right:
+                temp.append(arr[j])
+                j += 1
+
+            arr[left:right] = temp
+
+        merge_sort(0, n)
+
+        return result
