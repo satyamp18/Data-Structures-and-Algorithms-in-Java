738980d727010d2f69f78d53761ed15a9a8cc157
Satyam Pandey <satyampandey200317@gmail.com>
2026-09-04 18:09:50 +0530

    Time: 0 ms (100%), Space: 19.2 MB (93.1%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -0,0 +1,24 @@
+class Solution:
+    def firstStableIndex(self, nums, k):
+        n = len(nums)
+
+        # suffix_min[i] = minimum element from i to n-1
+        suffix_min = [0] * n
+        suffix_min[n - 1] = nums[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(nums[i], suffix_min[i + 1])
+
+        # prefix maximum
+        prefix_max = nums[0]
+
+        for i in range(n):
+            prefix_max = max(prefix_max, nums[i])
+
+            # instability score
+            score = prefix_max - suffix_min[i]
+
+            if score <= k:
+                return i
+
+        return -1
